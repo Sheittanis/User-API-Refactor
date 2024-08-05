6521031830cc23c33b2d289bc0a626585d5bef32
Savvas Sheittanis <17782124+Sheittanis@users.noreply.github.com>
2024-08-05 13:52:51 +0300

    Refactored controller and tests

--- Tests.User.Api/Tests.User.Api/Controllers/UserController.cs
+++ Tests.User.Api/Tests.User.Api/Controllers/UserController.cs
@@ -1,21 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Tests.User.Api.Controllers
 {
     public class UserController : Controller
     {
+        private readonly DatabaseContext _database;
+
+        public UserController(DatabaseContext database)
+        {
+            _database = database;
+        }
+
+        private async Task<IActionResult> SaveChangesInDB()
+        {
+            int result = await _database.SaveChangesAsync();
+
+            if (result > 0)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest("Failed to save");
+            }
+        }
+
+        private async Task<Models.UserModel> GetUser(int id)
+        {
+            return await _database.Users.Where(user => user.Id == id).FirstAsync();
+        }
+
         /// <summary>
         ///     Gets a user
         /// </summary>
         /// <param name="id">ID of the user</param>
         /// <returns></returns>
         [HttpGet]
-        [Route("api/users")]
-        public IActionResult Get(int id)
+        [Route("api/users/get")]
+        public async Task<IActionResult> Get(int id)
         {
-            DatabaseContext database = new DatabaseContext();
-            Models.User user = database.Users.Where(user => user.Id == id).First();
-            return Ok(user);
+            Models.UserModel user = await GetUser(id);
+
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return NotFound("User not found");
+            }
         }
 
         /// <summary>
@@ -26,19 +60,21 @@ namespace Tests.User.Api.Controllers
         /// <param name="age">Age of the user (must be a number)</param>
         /// <returns></returns>
         [HttpPost]
-        [Route("api/users")]
-        public IActionResult Create(string firstName, string lastName, string age)
+        [Route("api/users/create")]
+        public async Task<IActionResult> CreateUser(string firstName, string lastName, int age)
         {
-            DatabaseContext Database = new DatabaseContext();
-            Database.Users.Add(new Models.User
+            Models.UserModel newUser = new()
             {
                 Age = age,
                 FirstName = firstName,
                 LastName = lastName
-            });
-            Database.SaveChanges();
-            return Ok();
+            };
+
+            _database.Users.Add(newUser);
+
+            return await SaveChangesInDB();
         }
+
 
         /// <summary>
         ///     Updates a user
@@ -49,20 +85,27 @@ namespace Tests.User.Api.Controllers
         /// <param name="age">Age of the user (must be a number)</param>
         /// <returns></returns>
         [HttpPut]
-        [Route("api/users")]
-        public IActionResult Update(int id, string firstName, string lastName, string age)
+        [Route("api/users/update")]
+        public async Task<IActionResult> UpdateUser(int id, string firstName, string lastName, int age)
         {
-            DatabaseContext Database = new DatabaseContext();
-            Database.Users.Update(new Models.User
+            // Find the user
+            Models.UserModel user = await GetUser(id);
+
+            if (user == null)
             {
-                Age = age,
-                FirstName = firstName,
-                LastName = lastName,
-                Id = id
-            });
-            Database.SaveChanges();
-            return Ok();
+                return NotFound("User not found");
+            }
+
+            user.Age = age;
+            user.FirstName = firstName;
+            user.LastName = lastName;
+            user.Id = id;
+
+            _database.Users.Update(user);
+
+            return await SaveChangesInDB();
         }
+
 
         /// <summary>
         ///     Delets a user
@@ -70,16 +113,19 @@ namespace Tests.User.Api.Controllers
         /// <param name="id">ID of the user</param>
         /// <returns></returns>
         [HttpDelete]
-        [Route("api/users")]
-        public IActionResult Delete(int id)
+        [Route("api/users/delete")]
+        public async Task<IActionResult> Delete(int id)
         {
-            DatabaseContext database = new DatabaseContext();
-            database.Users.Remove(new Models.User
+            Models.UserModel user = await GetUser(id);
+
+            if (user == null)
             {
-                Id = id
-            });
-            database.SaveChanges();
-            return Ok();
+                return NotFound("User not found");
+            }
+
+            _database.Users.Remove(user);
+
+            return await SaveChangesInDB();
         }
     }
 }
